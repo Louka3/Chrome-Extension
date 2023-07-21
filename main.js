03732f445c66149befde995da175a4f72ac9486c
Lou-Laptop <lkuczykowski@gmail.com>
2023-07-21 17:45:15 -0400

    Added some of the logic to obtain the definitions in our fetch request

--- main.js
+++ main.js
@@ -2,17 +2,36 @@
 
 searchWord = function(word){
   // let query = word.selectionText;
+  console.log(word);
   let url = "https://api.dictionaryapi.dev/api/v2/entries/en/".concat(word.selectionText)
   fetch(url)
       .then(data => data.json())
       .then(data => {
-        const meaning = data[0]["meanings"]
-        
+        const meaning = data[0]["meanings"] // an array of all the objects inside of the meanings key
+        // need to get the length of the array, then loop through and extract the value at 'partOfSpeech'
+        // and the value at 'definition'
+        for(const obj of meaning){
+          const partOfSpeech = obj["partOfSpeech"] 
+          const definition = obj["definitions"]["definition"]
+          // create a string to display to the user. Contains the part of speech and the definition
+          const defString = `Part of Speech: ${partOfSpeech} \nDefinition: ${definition}`
+        }
+        /**
+         * How do we want to display the data? Our orignial idea was 
+         * to just edit the content of the initial popup from clicking on the extension, but that
+         * disappears from the screen.
+         * 
+         * - div with text content at the top?
+         * - browser alert?
+         * - can we somehow make a div appear close to the text selected by the user?
+         *    -> does javascipt somehow know the element of the word thats selected? 
+         * 
+         */
       });
     }
 
 chrome.contextMenus.create({
 title: "Search Definition",
 contexts:["selection"],  // ContextType
-onclick: searchWord // A callback function
+onclick: () => {searchWord(word)} // A callback function
 });
